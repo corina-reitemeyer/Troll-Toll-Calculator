7fdaf8c2ad29da4ac63e36562857a05ce328ab28
Corina Reitemeyer <53070641+corina-reitemeyer@users.noreply.github.com>
2024-06-12 14:48:53 +1200

    Write Pseudocode for solution

---
+++ client/components/Calculator.tsx
@@ -0,0 +1,16 @@
+export default function Calculator() {
+  // Corina's thinking
+  // Add 50 rockcandies to a total starting from 0
+  // if Rockcandies is < than 100 = display rockcandies (no conversion)
+  // if rockcandies is >= than 100 = display as 1 goldring (conversion)
+  // if Gold rings is < than 100 = display as goldrings (no conversion)
+  // if Gold rings is >= 100 = display 1 Goat (conversion)
+  // if Goats is >= 1 =  no further conversion, it stays as Goat (no conversion)
+  // --------------- //
+  // Bihui's thinking
+  // total = total + 50 rock candies
+  // if sum < 100 = rockcandies
+  // if 100 < total < 10000
+  // sum = total % 10
+  // if total > 10000, sum = 10000 % 100
+}
