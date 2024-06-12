be50730b1acc971179c0f8a1a5079da679b9cd85
Corina Reitemeyer <53070641+corina-reitemeyer@users.noreply.github.com>
2024-06-13 11:32:52 +1200

    React use state to include statechange of Troll-toll total

--- client/components/Calculator.tsx
+++ client/components/Calculator.tsx
@@ -1,35 +1,39 @@
 import { useState } from 'react'
 
 export default function Calculator() {
-  const total = 0
-  const goatTotal = Math.floor(total / 10000) // expect: 8
-  const goldRingTotal = Math.floor((total % 10000) / 100) //expect: 7
-  const rockCandyTotal = (total % 100000) % 100 // expect: 48
+  // const total = 0
+  // const goatTotal = Math.floor(total / 10000)
+  // const goldRingTotal = Math.floor((total % 10000) / 100)
+  // const rockCandyTotal = (total % 100000) % 100
 
-  const [activeTotal, setActiveTotal] = useState(total)
+  const [activeTotal, setActiveTotal] = useState(0)
+  const [activeGoldringTotal, setActiveGoalringTotal] = useState(0)
+  const [activeRockcandyTotal, setActiveRockcandyTotal] = useState(0)
+  const [activeGoatTotal, setActiveGoatTotal] = useState(0)
 
-  const handleEvent = (sum: number) => {
-    setActiveTotal(sum)
-
-    return (
-      <>
-        <hr></hr>
-        <h3>Toll Collection</h3>
-        <p>Bridge: Auckland Bridge</p>
-        {/* DATAPOINT NEEDED FROM DATABASE */}
-        <button onClick={() => handleEvent(activeTotal + 50)}>
-          Add toll charge
-        </button>
-        <hr></hr>
-        <section>
-          <h4>Troll-toll total</h4>
-          <p>incl. boogies and underbridge tax</p>
-          <hr></hr>
-          <p>Troll Rock Candy(Ȼ): {rockCandyTotal}</p>
-          <p>Gold Rings(AuR): {goldRingTotal}</p>
-          <p>Goat(GT): {goatTotal}</p>
-        </section>
-      </>
-    )
+  const handleEvent = () => {
+    setActiveTotal(activeTotal + 50)
+    setActiveGoatTotal(Math.floor((activeTotal + 50) / 10000))
+    setActiveGoalringTotal(Math.floor(((activeTotal + 50) % 10000) / 100))
+    setActiveRockcandyTotal(((activeTotal + 50) % 10000) % 100)
   }
+
+  return (
+    <>
+      <hr></hr>
+      <h3>Toll Collection</h3>
+      <p>Bridge:</p>
+      {/* DATAPOINT NEEDED FROM DATABASE */}
+      <button onClick={handleEvent}>Add toll charge</button>
+      <hr></hr>
+      <section>
+        <h4>Troll-toll total</h4>
+        <p>incl. boogies and underbridge tax</p>
+        <hr></hr>
+        <p>Troll Rock Candy(Ȼ): {activeRockcandyTotal}</p>
+        <p>Gold Rings(AuR): {activeGoldringTotal}</p>
+        <p>Goat(GT): {activeGoatTotal}</p>
+      </section>
+    </>
+  )
 }
