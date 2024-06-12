c27b9c908f67a6a24a1c16dd009cc359d1b04430
Hamiora Keelan <Hamiora.Keelan@gmail.com>
2024-06-13 06:54:10 +1200

    integrate with bridges

--- client/components/FavBridge.tsx
+++ client/components/FavBridge.tsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
 import { FavBridge } from '../../models/favBridge'
-// Use custom hook to make changes to the databases
 import useAddFavBridge from '../hooks/use-add-favBridge'
 import useDeleteFavBridge from '../hooks/use-delete-favBridge'
 
-function ChangeFavorite() {
+function ChangeFavorite({ bridgeId, userId }) {
   const [clicked, setClicked] = useState(false)
-  const [bridgeId, setBridgeId] = useState<number | null>(null)
+  const [favBridgeId, setFavBridgeId] = useState<number | null>(null)
 
   const addFavBridge = useAddFavBridge()
   const deleteFavBridge = useDeleteFavBridge()
@@ -14,35 +13,33 @@ function ChangeFavorite() {
   const handleAdd = () => {
     if (!clicked) {
       const newFavBridge: Omit<FavBridge, 'id'> = {
-        user_id: 1,
-        bridges_id: 1,
+        user_id: userId,
+        bridges_id: bridgeId,
       }
       addFavBridge.mutate(newFavBridge, {
         onSuccess: (data) => {
-          setBridgeId(data.id)
+          setFavBridgeId(data.id)
           setClicked(true)
         },
       })
-    } else if (bridgeId !== null) {
+    } else if (favBridgeId !== null) {
       const deleteData: Omit<FavBridge, 'id'> = {
-        bridges_id: 1,
-        user_id: 1,
+        bridges_id: bridgeId,
+        user_id: userId,
       }
       deleteFavBridge.mutate(deleteData, {
         onSuccess: () => {
           setClicked(false)
-          setBridgeId(null)
+          setFavBridgeId(null)
         },
       })
     }
   }
 
   return (
-    <>
-      <button onClick={handleAdd}>
-        {clicked ? 'Delete Favorite Bridge' : 'Add Favorite Bridge'}
-      </button>
-    </>
+    <button onClick={handleAdd}>
+      {clicked ? 'Delete Favorite Bridge' : 'Add Favorite Bridge'}
+    </button>
   )
 }
 
