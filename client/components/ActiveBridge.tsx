7dc540e063c9d7e2df6812f8e0bd32d9e8579e75
Hamiora Keelan <158783153+hamiora-keelan@users.noreply.github.com>
2024-06-14 09:16:39 +1200

    start front end component to handle active bridge

---
+++ client/components/ActiveBridge.tsx
@@ -0,0 +1,5 @@
+import { useState, UseEffect } from 'react'
+import { Bridge } from '../../models/bridge'
+import { UpdateActiveBridge } from '../hooks/use-update-activeBridge'
+
+import request from 'superagent'
--- client/hooks/api.ts
+++ client/hooks/api.ts
@@ -1,3 +1,4 @@
 export { default as useAddFavBridge } from './use-add-favBridge.ts'
 export { default as useDeleteFavBridge } from './use-delete-favBridge.ts'
 export { default as useDeleteFavBridge2 } from './use-delete-favBridge.ts'
+export { default as UpdateActiveBridge } from './use-add-favBridge.ts'
