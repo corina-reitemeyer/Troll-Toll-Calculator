69440bef8cd5c4eb663102075c130aebbccfea38
Conor Burke-Govey <21695840+cburkeg@users.noreply.github.com>
2024-06-14 11:22:05 +1200

    Add delete to bridges seeds; add onDelete attributes where missing

--- server/db/migrations/20240611223751_favBridges.js
+++ server/db/migrations/20240611223751_favBridges.js
@@ -1,13 +1,12 @@
 export async function up(knex) {
-    await knex.schema.createTable('favBridges', (table) => {
-      table.increments('id').primary()
-      table.integer('user_id').references('users.id').onDelete('cascade')
-      table.integer('bridges_id').references('bridges.id').onDelete('cascade')
-      // table.unique('')
-    })
-  }
-  
-  export async function down(knex) {
-    await knex.schema.dropTable('favBridges')
-  }
-  
+  await knex.schema.createTable('favBridges', (table) => {
+    table.increments('id').primary()
+    table.integer('user_id').references('users.id').onDelete('CASCADE')
+    table.integer('bridges_id').references('bridges.id').onDelete('CASCADE')
+    // table.unique('')
+  })
+}
+
+export async function down(knex) {
+  await knex.schema.dropTable('favBridges')
+}
--- server/db/migrations/20240611223958_tollAnalytics.js
+++ server/db/migrations/20240611223958_tollAnalytics.js
@@ -1,13 +1,12 @@
 export async function up(knex) {
-    await knex.schema.createTable('tollAnalytics', (table) => {
-      table.increments('id').primary()
-      table.integer('bridge_id').references('bridges.id')
-      table.datetime('timestamp')
-      table.decimal('revenue')
-    })
-  }
-  
-  export async function down(knex) {
-    await knex.schema.dropTable('tollAnalytics')
-  }
-  
+  await knex.schema.createTable('tollAnalytics', (table) => {
+    table.increments('id').primary()
+    table.integer('bridge_id').references('bridges.id').onDelete('CASCADE')
+    table.datetime('timestamp')
+    table.decimal('revenue')
+  })
+}
+
+export async function down(knex) {
+  await knex.schema.dropTable('tollAnalytics')
+}
