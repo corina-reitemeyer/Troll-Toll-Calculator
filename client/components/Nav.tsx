47fc6150c4cd3761b1c0cecbf18b619c7572bd9f
David Bowers Mason <163703657+david-bowers-mason@users.noreply.github.com>
2024-06-14 11:57:47 +1200

    fix troll patrol logo link

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -19,21 +19,21 @@ function Nav() {
   return (
     <>
       <div className="placeholder-nav-bar">
-        <img className="placeholder-banner-icon" src='img-url' alt='troll logo' />
+        <img src='/images/logo.png' alt='troll patrol logo' />
         <h2>TROLLTOLL PATROL</h2>
         <div className="menu-button">
           <Link to={``}>Bridges</Link>
         </div>
 
         <IfAuthenticated>
-          <button onClick={handleSignOut}>Sign out</button>
+          <button className="button-text" onClick={handleSignOut}>Sign out</button>
           <img width='200px'  className="profile-thumbnail" src='/images/Profile_pic_placeholder.png' alt='active troll' />
           {user && <p>You are signed in as {user?.nickname}</p>}
         </IfAuthenticated>
         
         <IfNotAuthenticated>
           <p>The user is NOT logged in !!!!!!</p>
-          <button onClick={handleSignIn}>Please sign in here</button>
+          <button className="button-text" onClick={handleSignIn}>Please sign in here</button>
         </IfNotAuthenticated>
       </div>
     </>
