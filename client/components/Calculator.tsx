08577393a00500dbb3fc05db893458c9458c435d
Corina Reitemeyer <53070641+corina-reitemeyer@users.noreply.github.com>
2024-06-13 15:54:26 +1200

    Add .tsx to calculator.tsx import

--- client/components/Calculator.tsx
+++ client/components/Calculator.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { sendRevenue } from '../apis/tollAnalytics'
-import Calculatorbanner from './CalculatorBanner'
+import Calculatorbanner from './CalculatorBanner.tsx'
 
 export default function Calculator() {
   const [activeTotal, setActiveTotal] = useState(0)
